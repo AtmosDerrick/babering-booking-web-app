e6d9ed44a81aeb3c5d2b9d80a32631fc6851d8a4
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2024-01-08 15:44:04 -0800

    adding fav button

--- src/pages/MainShop.jsx
+++ src/pages/MainShop.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import ImageCarousel from "../components/CarouselImages";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,9 +7,10 @@ import {
   faLayerGroup,
   faStar,
 } from "@fortawesome/free-solid-svg-icons";
-import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { faClock, faHeart } from "@fortawesome/free-regular-svg-icons";
 
 function MainShop() {
+  const [like, setLike] = useState(false);
   const shop = {
     id: 3,
     name: "Style Haven",
@@ -39,7 +40,36 @@ function MainShop() {
                   </h2>
                   <h3 className="text-xs">{shop.location}</h3>
                 </div>
-                <div className="w-1/4">
+                <div className="w-2/4 flex justify-start items-center gap-4">
+                  <h3
+                    className="text-sm"
+                    onClick={() => {
+                      setLike(true);
+                    }}>
+                    {like ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6 text-primary drop-shadow-sm">
+                        <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-primary">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                        />
+                      </svg>
+                    )}
+                  </h3>{" "}
                   <h3 className="text-sm">
                     <FontAwesomeIcon
                       icon={faStar}
